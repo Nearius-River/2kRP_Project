fecd8967291680ea6525a1cd3589de6454ae9868
Near <49107257+Nearius-River@users.noreply.github.com>
2024-06-07 15:56:03 -0300

    Added constants.py to store useful app data

---
+++ 2kRP_Presence/utils/constants.py
@@ -0,0 +1,10 @@
+app_data = {
+    'app_name': '2kRP',
+    'app_version': '1.1'
+}
+
+def get_app_name():
+    return app_data['app_name']
+
+def get_app_version():
+    return app_data['app_version']
