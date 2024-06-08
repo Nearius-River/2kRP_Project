5596ba828b6a7c7e0d0b25270179278473fd9e1b
Near <49107257+Nearius-River@users.noreply.github.com>
2024-06-08 01:52:31 -0300

    Code review

--- 2kRP_Extension/popup.js
+++ 2kRP_Extension/popup.js
@@ -1,15 +1,29 @@
-function checkServerStatus(url) {
-    return fetch(url)
-        .then(response => response.ok)
-        .catch(error => {
-            console.error('Error checking server status:', error);
-            return false;
-        });
+// popup.js
+const SERVER_URL = 'http://localhost:7789/status';
+
+async function checkServerStatus(url) {
+    try {
+        const response = await fetch(url);
+        return response.ok;
+    } catch (error) {
+        console.error('Error checking server status:', error);
+        return false;
+    }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleSwitch = document.getElementById('toggle-switch');
+function updateServerStatus(isServerActive) {
     const serverStatus = document.getElementById('server-status');
+    if (isServerActive) {
+        serverStatus.textContent = 'Server is up and running!';
+        serverStatus.style.color = 'green';
+    } else {
+        serverStatus.textContent = 'Server is inactive! Be sure the receiver app is running.';
+        serverStatus.style.color = 'red';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', async function () {
+    const toggleSwitch = document.getElementById('toggle-switch');
 
     // Restore the switch state from storage
     chrome.storage.sync.get('extensionEnabled', function (data) {
@@ -17,30 +31,19 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     // Check server status
-    checkServerStatus('http://localhost:7789/status').then(isServerActive => {
-        if (isServerActive) {
-            serverStatus.textContent = 'Server is up and running!';
-            serverStatus.style.color = 'green';
-        } else {
-            serverStatus.textContent =
-                'Server is inactive! Be sure the receiver app is running.';
-            serverStatus.style.color = 'red';
-        }
-    });
+    const isServerActive = await checkServerStatus(SERVER_URL);
+    updateServerStatus(isServerActive);
 
     toggleSwitch.addEventListener('change', function () {
         const isEnabled = toggleSwitch.checked;
         chrome.storage.sync.set({ extensionEnabled: isEnabled });
 
         // Send message to content script to start or stop the collection
-        chrome.tabs.query(
-            { active: true, currentWindow: true },
-            function (tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, {
-                    type: 'TOGGLE_EXTENSION',
-                    enabled: isEnabled,
-                });
-            }
-        );
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                type: 'TOGGLE_EXTENSION',
+                enabled: isEnabled,
+            });
+        });
     });
 });
