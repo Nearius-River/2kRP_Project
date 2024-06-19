bcecc76715988e34304aa168f4e104b5762bddff
Near <49107257+Nearius-River@users.noreply.github.com>
2024-06-19 11:48:43 -0300

    Encapsulate data_store into a class instead of relying on global

--- 2kRP_Presence/shared/data.py
+++ 2kRP_Presence/shared/data.py
@@ -1,21 +1,33 @@
-data_store = {
-    'gameType': None,
-    'location': None,
-    'badgeImageUrl': None,
-    'playersOnline': '0',
-    'playersOnMap': '0',
-    'wikiPageUrl': None
-}
+class DataStore:
+    def __init__(self):
+        self._data_store = {
+            'gameType': None,
+            'location': None,
+            'badgeImageUrl': None,
+            'playersOnline': '0',
+            'playersOnMap': '0',
+            'wikiPageUrl': None
+        }
+
+    def update_data(self, gameType, location, badgeImageUrl, playersOnline, playersOnMap, wikiPageUrl):
+        """Update storage data."""
+        self._data_store['gameType'] = gameType
+        self._data_store['location'] = location
+        self._data_store['badgeImageUrl'] = badgeImageUrl
+        self._data_store['playersOnline'] = playersOnline
+        self._data_store['playersOnMap'] = playersOnMap
+        self._data_store['wikiPageUrl'] = wikiPageUrl
+
+    def get_data(self):
+        """Get storage data."""
+        return self._data_store
+
+data_store = DataStore()
 
 def update_data(gameType, location, badgeImageUrl, playersOnline, playersOnMap, wikiPageUrl):
-    global data_store
-    data_store['gameType'] = gameType
-    data_store['location'] = location
-    data_store['badgeImageUrl'] = badgeImageUrl
-    data_store['playersOnline'] = playersOnline
-    data_store['playersOnMap'] = playersOnMap
-    data_store['wikiPageUrl'] = wikiPageUrl
+    """Global function to update data in store."""
+    data_store.update_data(gameType, location, badgeImageUrl, playersOnline, playersOnMap, wikiPageUrl)
 
 def get_data():
-    global data_store
-    return data_store
+    """Global function to retrieve data."""
+    return data_store.get_data()
