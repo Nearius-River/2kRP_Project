893ed3ecd4ee24853ffc3e32445562aa6ff526e0
nearius-river <minecom4321@hotmail.com>
2025-05-07 18:39:54 -0300

    Game compatibility check removed. Deemed unecessary, since all games share the same page layout.

--- 2kRP_Extension/content.js
+++ 2kRP_Extension/content.js
@@ -1,26 +1,4 @@
 let intervalId = null;
-const supportedGameTypes = [
-    '2kki',
-    'amillusion',
-    'braingirl',
-    'deepdreams',
-    'flow',
-    'genie',
-    'if',
-    'mikan',
-    'muma',
-    'nostalgic',
-    'oneshot',
-    'oversomnia',
-    'prayers',
-    'sheawaits',
-    'someday',
-    'tsushin',
-    'ultraviolet',
-    'unconscious',
-    'unevendream',
-    'yume'
-];
 
 /**
  * Extracts the first number found in a string
@@ -28,24 +6,17 @@ const supportedGameTypes = [
  * @returns {string}
  */
 function extractCount(inputString) {
-    const match = inputString.match(/\d+/);
-    return match ? match[0] : '0';
+  const match = inputString.match(/\d+/);
+  return match ? match[0] : "0";
 }
 
 /**
- * Determines the game type based on the URL path
+ * Returns only the game ID from the URL path
  * @returns {string|null}
  */
 function getGameType() {
-    const pathname = window.location.pathname;
-    
-    for (const gameType of supportedGameTypes) {
-        if (pathname.startsWith(`/${gameType}`)) {
-            return gameType;
-        }
-    }
-    
-    return null;
+  const pathname = window.location.pathname.replace(/\//g, "");;
+  return pathname;
 }
 
 /**
@@ -53,17 +24,17 @@ function getGameType() {
  * @returns {{location: string|null, wikiPageUrl: string|null}}
  */
 function getLocationData() {
-    const spanElement = document.getElementById('locationText');
-    if (spanElement) {
-        const linkElement = spanElement.querySelector('a');
-        if (linkElement) {
-            return {
-                location: linkElement.innerText,
-                wikiPageUrl: linkElement.href
-            };
-        }
+  const spanElement = document.getElementById("locationText");
+  if (spanElement) {
+    const linkElement = spanElement.querySelector("a");
+    if (linkElement) {
+      return {
+        location: linkElement.innerText,
+        wikiPageUrl: linkElement.href,
+      };
     }
-    return { location: null, wikiPageUrl: null };
+  }
+  return { location: null, wikiPageUrl: null };
 }
 
 /**
@@ -72,8 +43,8 @@ function getLocationData() {
  * @returns {number}
  */
 function getPlayerCount(elementId) {
-    const element = document.getElementById(elementId);
-    return element ? parseInt(extractCount(element.innerText)) : 0;
+  const element = document.getElementById(elementId);
+  return element ? parseInt(extractCount(element.innerText)) : 0;
 }
 
 /**
@@ -82,76 +53,79 @@ function getPlayerCount(elementId) {
  * @returns {string|null}
  */
 function getBackgroundImageUrl(element) {
-    const style = window.getComputedStyle(element);
-    const backgroundImage = style.backgroundImage;
-    const urlMatch = backgroundImage.match(/url\(["']?([^"']*)["']?\)/);
-    return urlMatch ? urlMatch[1] : null;
+  const style = window.getComputedStyle(element);
+  const backgroundImage = style.backgroundImage;
+  const urlMatch = backgroundImage.match(/url\(["']?([^"']*)["']?\)/);
+  return urlMatch ? urlMatch[1] : null;
 }
 
 /**
  * Sends all collected data to the background script
  */
 function sendAllData() {
-    const gameType = getGameType()
-
-    if (supportedGameTypes.includes(gameType) || gameType == null) {
-        const { location, wikiPageUrl } = getLocationData();
-        const playersOnline = getPlayerCount('playerCountLabel');
-        const playersOnMap = getPlayerCount('mapPlayerCountLabel');
-        let badgeImageUrl = null;
-        const badgeElement = document.querySelector('#badgeButton .badge');
-    
-        if (badgeElement) {
-            badgeImageUrl = getBackgroundImageUrl(badgeElement);
-        }
-    
-        chrome.runtime.sendMessage({
-            type: '2KKI_DATA',
-            gameType,
-            location,
-            badgeImageUrl,
-            playersOnline,
-            playersOnMap,
-            wikiPageUrl
-        });
-    } else {
-        return console.warn('Current game is not supported! Data not sent.')
+  const gameType = getGameType();
+  chrome.storage.sync.get(gameType, function (data) {
+    if (data[gameType] === false) {
+      return console.warn(
+        "Current game is disabled for presence! Data not sent."
+      );
     }
+
+    const { location, wikiPageUrl } = getLocationData();
+    const playersOnline = getPlayerCount("playerCountLabel");
+    const playersOnMap = getPlayerCount("mapPlayerCountLabel");
+    let badgeImageUrl = null;
+    const badgeElement = document.querySelector("#badgeButton .badge");
+
+    if (badgeElement) {
+      badgeImageUrl = getBackgroundImageUrl(badgeElement);
+    }
+
+    chrome.runtime.sendMessage({
+      type: "2KKI_DATA",
+      gameType,
+      location,
+      badgeImageUrl,
+      playersOnline,
+      playersOnMap,
+      wikiPageUrl,
+    });
+  });
 }
 
 /**
  * Starts the data collection at regular intervals
  */
 function startCollecting() {
-    if (!intervalId) {
-        intervalId = setInterval(sendAllData, 15000);
-    }
+  if (!intervalId) {
+    intervalId = setInterval(sendAllData, 5000);
+  }
 }
 
 /**
  * Stops the data collection
  */
 function stopCollecting() {
-    if (intervalId) {
-        clearInterval(intervalId);
-        intervalId = null;
-    }
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+  }
 }
 
 // Listens for messages to toggle the extension's data collection
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === 'TOGGLE_EXTENSION') {
-        if (message.enabled) {
-            startCollecting();
-        } else {
-            stopCollecting();
-        }
+  if (message.type === "TOGGLE_EXTENSION") {
+    if (message.enabled) {
+      startCollecting();
+    } else {
+      stopCollecting();
     }
+  }
 });
 
 // Initializes the extension based on the stored enabled state
-chrome.storage.sync.get('extensionEnabled', function (data) {
-    if (data.extensionEnabled !== false) {
-        startCollecting();
-    }
+chrome.storage.sync.get("extensionEnabled", function (data) {
+  if (data.extensionEnabled !== false) {
+    startCollecting();
+  }
 });
